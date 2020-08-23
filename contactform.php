9d8a67163ec3d7b25887485a6755715bb0160c79
Katrina H <katrina.hitchcock@gmail.com>
2020-08-23 14:33:00 +0200

    update contact form

--- contactform.php
+++ contactform.php
@@ -2,17 +2,17 @@
 
 if(isset($_POST['submit'])) {
 
-    $name = $POST['name'];
-    $subject = $POST['subject'];
-    $mailForm = $POST['mail'];
-    $message = $POST['message'];
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
     $mailTo = 'info@trina.dev';
-    $headers = 'From: '.$mailFrom;
-    $txt = "You have recieved an email from ".$name.".\n\n".$message;
+    $headers = 'From: ' .$mailFrom;
+    $txt = "You have recieved an email from: \n\n" .$name ."\n\n" .$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header('Location:index.php?mailsend');
+    // header('Location:index.html?mailsend');
 }
 
 ?>
---
+++ script.js
@@ -0,0 +1,24 @@
+const enquiry = document.querySelector('.enquiry')
+const form = document.querySelector('.contact-form')
+const email = document.getElementById('email')
+
+// Check email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (re.test(input.value.trim())) {
+    showSuccess(input)
+  } else {
+    showError(input, 'Email is not valid')
+  }
+}
+
+// Event listeners
+form.addEventListener('submit', function (e) {
+  // e.preventDefault()
+
+  //   checkEmail(email)
+  const form = (document.getElementById('enquiry').className =
+    'contact-form sent')
+
+  console.log('submit clicked')
+})
