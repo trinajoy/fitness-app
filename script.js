e78009ddd090a6b0bfac71b566b584594995db06
Katrina H <katrina.hitchcock@gmail.com>
2020-09-06 13:56:17 +0200

    update script for shared nav

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const enquiry = document.querySelector('.enquiry')
 const form = document.querySelector('.contact-form')
 const email = document.getElementById('email')
 
+window.addEventListener('load', (event) => {
+  console.log('The page has fully loaded')
+  $('#navbar-placeholder').load('../assets/nav.html')
+})
+
 // Check email is valid
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -16,7 +21,6 @@ function checkEmail(input) {
 form.addEventListener('submit', function (e) {
   // e.preventDefault()
 
-  //   checkEmail(email)
   const form = (document.getElementById('enquiry').className =
     'contact-form sent')
 
